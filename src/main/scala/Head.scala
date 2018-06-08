f7f4385d4c8707cf61423f939776b4600baf13c0
Yordan Angelov <yordan.angelov@ten10.com>
2018-06-08 09:30:05 +0100

    added solutions for filter using recursion, for comprehension, and foldLeft

--- src/main/scala/Head.scala
+++ src/main/scala/Head.scala
@@ -1,5 +1,33 @@
 package reinvent
 
 object Head {
-  def apply(input: List[Int]): Int = ???
+  def apply[A](input: List[A]): A = input match {
+    case Nil ⇒ throw new RuntimeException("Head of empty list")
+    case (h :: _) ⇒ h
+  }
+
+}
+
+object Filter {
+
+  //  Look at using Recursion, foldLeft, collect
+  //  def apply[A](input: List[A], predicate: A ⇒ Boolean): List[A] = ???
+
+  def apply[A](input: List[A], predicate: A ⇒ Boolean): List[A] = {
+    def recurse(input: List[A], acc: List[A]): List[A] = {
+      if (input.isEmpty) acc
+      else if (predicate(input.head)) recurse(input.tail, acc :+ input.head)
+      else recurse(input.tail, acc)
+    }
+
+    recurse(input, List())
+  }
+
+  def apply[A](input: List[A], predicate: A ⇒ Boolean): List[A] =
+    for (a ← input if predicate(a)) yield a
+
+  def apply[A](input: List[A], predicate: A ⇒ Boolean): List[A] =
+    input.foldLeft(List[A]()) { (a: List[A], b: A) ⇒
+      if (predicate(b)) b::a else a}.reverse
+
 }
--- src/test/scala/HeadTest.scala
+++ src/test/scala/HeadTest.scala
@@ -10,10 +10,29 @@ import org.scalatest.prop.PropertyChecks
 class HeadTest extends FunSuite with Matchers with PropertyChecks {
 
   test("Head should exist in the input") {
-    forAll{ input: List[Int] =>
-      whenever (input.nonEmpty) {
-        input.contains(Head(input)) should be (true)
+    forAll { input: List[Int] =>
+      whenever(input.nonEmpty) {
+        input.contains(Head(input)) should be(true)
       }
+    }
+  }
+
+  test("Filter should return a list smaller or equal in size to the input") {
+    forAll { (input: List[Int], pred: Boolean) => {
+      {Filter(input, { x: Int ⇒ pred }).size <= input.size} should be(true)
+      }
+    }
+  }
+
+  test("Filter should return the input if the predicate is always true") {
+    forAll { input: List[Int] =>
+      Filter(input, { x: Int ⇒ true }) should be(input)
+    }
+  }
+
+  test("Filter should nil if the predicate is always false") {
+    forAll { input: List[Int] =>
+      Filter(input, { x: Int ⇒ false }) should be(Nil)
     }
   }
 }
